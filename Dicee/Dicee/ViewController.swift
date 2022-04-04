3a0928e123e4fe79d95df25133e62e0b1aa0a1fd
Lily O'Sullivan <lilyosullivan@pm.me>
2022-04-04 17:10:56 +0100

    ✨ Added Outlets and Actions to dice screen elements

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageViewLeft.image = UIImage(named: "DiceSix")
+        diceImageViewRight.image = UIImage(named:"DiceTwo")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewLeft.image = UIImage(named: "DiceFour")
+        diceImageViewRight.image = UIImage(named:"DiceFour")
+    }
+    
 }
 
